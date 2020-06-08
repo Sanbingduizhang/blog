4f880d5487baa227775bf0cb2d4a1c14f200e646
fengxinlong <18862005869@163.com>
2020-06-08 16:48:36 +0800

    xiugai

--- config/blog.php
+++ config/blog.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'name' => "冯小六",
-    'title' => "冯小六的一些事",
+    'title' => "冯小六的学习笔记",
     'subtitle' => 'https://article.liawen.top',
     'description' => '冯小六的学习笔记',
     'author' => '冯小六',
@@ -11,4 +11,6 @@ return [
         'storage' => 'public',
         'webpath' => '/storage/uploads',
     ],
+    'xue_yuan_jun' => 'https://xueyuanjun.com/books/laravel-blog-tutorial/chapter/laravel-blog-5_7',
+    'github' => 'https://github.com/Sanbingduizhang/blog',
 ];
